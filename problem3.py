e4abba97134632572022f2e79dbc67f0859ee9ac
joshuahomem <71406869+joshuahomem@users.noreply.github.com>
2020-10-06 12:50:53 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,16 +1,14 @@
+  
 #! python3
 """
 Ask the user to enter positive integers.
 After each entry, add the number to a list
 If the entry is -1 then stop adding numbers to the list
 Sort the list and display the highest number added
-
 inputs:
 as many integers as needed
-
 outputs:
 Display the largest number:
-
 examples:
 Enter an integer:3
 Enter an integer:2
@@ -20,17 +18,12 @@ Enter an integer:48
 Enter an integer:13
 Enter an integer:24
 Enter an integer:-1
-
 The largest number you entered is 92
 """
-
-b=[]
-a=""
-while a!="-1":
-    a=input("Enter in an interger:")
-    
-    b.append(int(a))
-    b.sort()
-if int(a)==-1:
-    b.sort()
-    print(b[-1])
+list1=[]
+while (-1 in list1)==False:
+    in1=int(input("Enter an integer: "))
+    list1.append(in1)
+if (-1 in list1)==True:
+    list1.sort()
+    print("The largest number you entered is "+str(list1[-1]))
